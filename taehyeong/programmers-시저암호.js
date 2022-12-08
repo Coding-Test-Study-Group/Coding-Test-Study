668ef5a3387deddc85356624e3193b55fb2ac104
이태형 <spdhsrnvl123@naver.com>
2022-12-09 01:40:53 +0900

    📝프로그래머스 시저암호

---
+++ taehyeong/programmers-시저암호.js
@@ -0,0 +1,10 @@
+function solution(s, n) {
+    var answer = '';
+    for(let i=0;i<s.length;i++){
+        if(s[i] !== " "){
+            if(s.charCodeAt(i) ===
+            console.log(s.charCodeAt(i) + n)
+        }
+    }
+    return answer;
+}
