c02e2ef8910ecca438d62da36679a37149ed7335
이태형 <spdhsrnvl123@naver.com>
2022-12-09 21:24:42 +0900

    📝프로그래머스 시저암호

--- taehyeong/programmers-시저암호.js
+++ taehyeong/programmers-시저암호.js
@@ -1,10 +1,20 @@
 function solution(s, n) {
-    var answer = '';
+    let CodeArr = [];
     for(let i=0;i<s.length;i++){
-        if(s[i] !== " "){
-            if(s.charCodeAt(i) ===
-            console.log(s.charCodeAt(i) + n)
+        let Acode = s.charCodeAt(i);
+        console.log(Acode)
+        //소문자일 경우
+        if(Acode > 96 && Acode < 123){
+            Acode + n > 122 ? CodeArr.push((Acode + n) - 26) : CodeArr.push(Acode+n)
         }
+        //대문자일 경우
+        if(Acode > 64 && Acode < 91){
+            Acode + n > 90 ? CodeArr.push((Acode + n) - 26) : CodeArr.push(Acode+n) 
+        }
+        //공백일 경우
+        if(Acode === 32){
+            CodeArr.push(Acode)
+        }        
     }
-    return answer;
+    return String.fromCharCode(...CodeArr)
 }
