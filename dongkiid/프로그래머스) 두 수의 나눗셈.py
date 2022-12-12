1359733585022d34542470ff937931dde8e916e7
dongkiid <mindo7211@daum.net>
2022-12-12 19:35:32 +0900

    프로그래머스) 각도기, 배열두배만들기, 두 수의 나눗셈

---
+++ dongkiid/프로그래머스) 두 수의 나눗셈.py
@@ -0,0 +1,5 @@
+def solution(num1, num2):
+    num1 =int(num1)
+    num2 =int(num2)
+    answer = int((num1 / num2) *1000)
+    return answer
---
+++ dongkiid/프로그래머스) 배열 두배 만들기.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    answer = []
+    for i in numbers:
+        answer.append(i*2)
+    return answer
