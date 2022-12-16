2e97e9f4ee75c918943a6b3c74b8e40e8ebbf67e
김지수 <wltn2858@swu.ac.kr>
2022-12-17 01:44:39 +0900

    프로그래머스 중복된 문자 제거

---
+++ soojik/programmers/src/중복된_문자_제거.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = ""
+
+        // 문자열 하나하나 순회하며 answer에 해당 문자가 없다면 추가하고, 아니면 수항 작업 없도록
+        for (s in my_string) {
+            if (answer.indexOf(s) < 0) {
+                answer += s;
+            }
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    println(Solution().solution("people"));
+    println(Solution().solution("We are the world"));
+}
