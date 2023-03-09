da7c7d61cafacf6dc70e4d8d1f1a047c55696982
Hyyena <sjtxm0320@gmail.com>
2023-03-10 02:31:30 +0900

    프로그래머스 이상한 문자 만들기

--- hyeonwoo/Python/프로그래머스/프로그래머스 이상한 문자 만들기.py
+++ hyeonwoo/Python/프로그래머스/프로그래머스 이상한 문자 만들기.py
@@ -15,21 +15,21 @@ def solution(s):
     answer = ""
 
     # 단어를 기준으로 몇 번째 문자인지 확인하는 변수
-    idx = 1
+    idx = 0
     for character in s:
-        # 문자가 공백이라면 정답 변수에 추가한 뒤 인덱스를 1로 만든 뒤 다음 반복문 실행
+        # 문자가 공백이라면 정답 변수에 추가한 뒤 인덱스를 0으로 만든 뒤 다음 반복문 실행
         if character == " ":
             answer += character
-            idx = 1
+            idx = 0
             continue
 
-        # 문자열이 홀수번째라면 대문자로 바꾼 뒤 정답 변수에 추가
+        # 문자열이 홀수번째라면 소문자로 바꾼 뒤 정답 변수에 추가
         if is_odd(idx):
-            answer += character.upper()
-
-        # 문자열이 짝수번째라면 소문자로 바꾼 뒤 정답 변수에 추가
-        if is_even(idx):
             answer += character.lower()
+
+        # 문자열이 짝수번째라면 대문자로 바꾼 뒤 정답 변수에 추가
+        if is_even(idx):
+            answer += character.upper()
 
         # 다음 반복문 실행 전 인덱스를 증가시켜준다.
         idx += 1
