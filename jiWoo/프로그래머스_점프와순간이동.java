09de6de5610a1c1344ab6b6e00f3afee20b3db4a
j2woo <kjk04021@naver.com>
2023-03-26 15:57:28 +0900

    프로그래머스- 점프와 순간 이동

---
+++ jiWoo/프로그래머스_점프와순간이동.java
@@ -0,0 +1,20 @@
+package jiWoo;
+
+public class 프로그래머스_점프와순간이동 {
+        // 배수의 문제로 홀수는 전 짝수의 +1씩 늘리므로
+        //2씩 나누며 홀수를 만났을 때의 count만 체크해준다. (count만큼 +1)
+        public int solution(int n) {
+            int ans = 1;
+            int cnt=0; // +1되는 숫자
+            while(n>2){
+                if(n%2==1){ // n이 홀수면 +1해주기 위해 cnt++;
+                    n-=1;
+                    cnt++;
+                }
+                n/=2;
+            }
+            ans+=cnt;
+            return ans;
+        }
+
+}
